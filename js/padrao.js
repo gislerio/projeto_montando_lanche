954d7d37fc5eadcf1cb9e5319d3150bfd577985c
Gislerio Junior <gisleriojunior@hotmail.com>
2022-08-03 22:42:03 -0300

    propriedades computadas do vue

--- js/padrao.js
+++ js/padrao.js
@@ -1,4 +1,24 @@
 const vm = new Vue({
-    el:'#app'
+    el: '#app',
+    computed: {
+        pao() {
+            return ['./img/padrao/pao_superior.png', './img/padrao/pao_inferior.png']
+        },
+        alface() {
+            return './img/padrao/alface.png'
+        },
+        ketchup() {
+            return './img/padrao/molho.png'
+        },
+        Mostarda() {
+            return './img/padrao/molho.png'
+        },
+        Maionese() {
+            return './img/padrao/molho.png'
+        },
+        hamburguer() {
+            return './img/padrao/hamburguer.png'
+        },
+    }
 
 });
